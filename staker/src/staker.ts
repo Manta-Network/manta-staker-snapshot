b9d8fe030b5f5476a0bfaefc4129aafe25af190b
wuyahuang <wuyahuang@gmail.com>
2024-01-25 11:06:21 +0800

    Initialize subgraph

---
+++ staker/src/staker.ts
@@ -0,0 +1,33 @@
+import {
+  BindPacificAddress as BindPacificAddressEvent,
+  Initialized as InitializedEvent
+} from "../generated/Staker/Staker"
+import { BindPacificAddress, Initialized } from "../generated/schema"
+
+export function handleBindPacificAddress(event: BindPacificAddressEvent): void {
+  let entity = new BindPacificAddress(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.atlanticAddress = event.params.atlanticAddress
+  entity.pacificAddress = event.params.pacificAddress
+  entity.blockNumber = event.params.blockNumber
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleInitialized(event: InitializedEvent): void {
+  let entity = new Initialized(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.version = event.params.version
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ staker/tests/staker-utils.ts
@@ -0,0 +1,49 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import { BindPacificAddress, Initialized } from "../generated/Staker/Staker"
+
+export function createBindPacificAddressEvent(
+  atlanticAddress: string,
+  pacificAddress: Address,
+  blockNumber: BigInt
+): BindPacificAddress {
+  let bindPacificAddressEvent = changetype<BindPacificAddress>(newMockEvent())
+
+  bindPacificAddressEvent.parameters = new Array()
+
+  bindPacificAddressEvent.parameters.push(
+    new ethereum.EventParam(
+      "atlanticAddress",
+      ethereum.Value.fromString(atlanticAddress)
+    )
+  )
+  bindPacificAddressEvent.parameters.push(
+    new ethereum.EventParam(
+      "pacificAddress",
+      ethereum.Value.fromAddress(pacificAddress)
+    )
+  )
+  bindPacificAddressEvent.parameters.push(
+    new ethereum.EventParam(
+      "blockNumber",
+      ethereum.Value.fromUnsignedBigInt(blockNumber)
+    )
+  )
+
+  return bindPacificAddressEvent
+}
+
+export function createInitializedEvent(version: BigInt): Initialized {
+  let initializedEvent = changetype<Initialized>(newMockEvent())
+
+  initializedEvent.parameters = new Array()
+
+  initializedEvent.parameters.push(
+    new ethereum.EventParam(
+      "version",
+      ethereum.Value.fromUnsignedBigInt(version)
+    )
+  )
+
+  return initializedEvent
+}
