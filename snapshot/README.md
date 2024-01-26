777e76bdb7f4b9e6aab62ddec78e50ccbfa879be
wuyahuang <wuyahuang@gmail.com>
2024-01-26 18:24:58 +0800

    update snapshot to support query staking amount

---
+++ snapshot/README.md
@@ -0,0 +1,19 @@
+# Generate snapshot for atlantic stakers
+
+### Install Node.js packages
+
+```
+yarn
+```
+
+### Update snapshotBlockNumber of index.mjs
+
+```
+const snapshotBlockNumber = 1204059;
+```
+
+### Execute script
+
+```
+node index.mjs
+```
--- snapshot/index.mjs
+++ snapshot/index.mjs
@@ -1,21 +1,32 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+
+const snapshotBlockNumber = 1204059;
 
 let skip = 0;
 let first = 500;
 let bindRecords = {};
 
-// snapshot file
-const writeStream = fs.createWriteStream("./snapshot.csv", { encoding: "utf-8" });
+const main = async () => {
+  // Construct API provider
+  const wsProvider = new WsProvider('wss://ws.manta.systems');
+  const api = await ApiPromise.create({ provider: wsProvider, noInitWarn: true });
 
-writeStream.on("open", async () => {
-  writeStream.write("pacificAddress,atlanticAddress,stakedBlockNumber\n", "utf-8");
-  queryStakedRecord();
-});
+  // generate snapshot file
+  const writeStream = fs.createWriteStream("./snapshot.csv", { encoding: "utf-8" });
 
-async function queryStakedRecord() {
+  writeStream.on("open", async () => {
+    writeStream.write("pacificAddress,atlanticAddress,bindBlockNumber,stakingAmount\n", "utf-8");
+    await queryStakedRecord(api, writeStream);
+  });
+}
+
+main();
+
+async function queryStakedRecord(api, writeStream) {
   //set the query url
-  var queryURL = "https://api.goldsky.com/api/public/project_clnv4qr7e30dv33vpgx7y0f1d/subgraphs/testnet-staker/1.0.6/gn"
+  var queryURL = "https://api.goldsky.com/api/public/project_clnv4qr7e30dv33vpgx7y0f1d/subgraphs/mainnet-staker/1.0.0/gn"
 
   //define the query to fetch a list of stake record
   const graphQuery = `query {
@@ -36,6 +47,7 @@ async function queryStakedRecord() {
     })
   };
 
+  console.log(`query bind records from subgraph, first:${first}, skip:${skip}`);
   //get the response
   const response = await fetch(queryURL, options);
 
@@ -45,8 +57,35 @@ async function queryStakedRecord() {
   // override old bind record
   const records = queryResult.data.bindPacificAddresses;
   records.map((item) => {
-    bindRecords[item.atlanticAddress] = item;
+    if (item.blockNumber <= snapshotBlockNumber) {
+      item.stakingAmount = 0;
+      bindRecords[item.atlanticAddress] = item;
+    }
   })
+
+  let atlanticAddressArray = Object.keys(bindRecords);
+
+  console.log(`query staking amount on Atlantic network, first:${first}, skip:${skip}`);
+  // query staking amount
+  const delegatorState = await api.query.parachainStaking.delegatorState.multi(
+    atlanticAddressArray
+  );
+
+  for (let i = 0; i < atlanticAddressArray.length; i++) {
+    const currentDelegatorState = delegatorState[i];
+
+    const delegationsRaw = currentDelegatorState.isSome
+      ? currentDelegatorState.value.delegations
+      : [];
+
+    let currentAccountStakingAmount = 0;
+    await delegationsRaw.map((delegationRaw) => {
+      currentAccountStakingAmount = Number(currentAccountStakingAmount) + parseInt(delegationRaw.amount / 1e18);
+    });
+
+    // update staking amount of bind record
+    bindRecords[atlanticAddressArray[i]].stakingAmount = currentAccountStakingAmount;
+  }
 
   // fetch next records
   if (records.length == first) {
@@ -59,10 +98,13 @@ async function queryStakedRecord() {
       records.push(value.pacificAddress);
       records.push(value.atlanticAddress);
       records.push(value.blockNumber);
+      records.push(value.stakingAmount);
 
       const toString = records.join(",")
 
       writeStream.write(toString + "\n", "utf-8");
     }
+    console.log(`snapshot records saved into snapshot.csv`);
+    await api.disconnect();
   }
 }
